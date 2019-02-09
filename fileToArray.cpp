76466029c730764b14339185d41d602785b5699c
markstein98 <marco.aliberti98@gmail.com>
2019-02-09 18:07:32 +0100

    Adding basic functions

--- fileToArray.cpp
+++ fileToArray.cpp
@@ -1,1 +1,51 @@
+#include "fileToArray.h"
 
+MyData::MyData(const char* filename){
+	_ndati = countData(filename);
+	_data = readData(filename);
+}
+
+FILE* MyData::myfopen(const char *filename){
+	FILE* fp;
+	fp = fopen(filename, "r");
+	if(fp == NULL){
+		fprintf(stderr,"Error while opening file %s", filename);
+		fprintf(stderr,"\nPress any key to exit.\n");
+		fflush(stdin);
+		getchar();
+		exit(1);
+	}
+	return fp;
+}
+
+int MyData::countData(const char* filename){
+	FILE* file;
+	int n;
+	float swap;
+	
+	file = myfopen(filename);
+	for(n=0;fscanf("%f",&swap)==1;n++) {}
+	fclose(file);
+	return n;
+}
+
+float* MyData::readData(const char* filename){
+	FILE* file;
+	float* dataArray;
+	
+	dataArray = (float*) malloc(_ndati * sizeof(float));
+	file = myfopen(filename);
+	for(int i=0;i<_ndati;i++){
+		fscanf(file,"%f",&dataArray[i]);
+	}
+	fclose(file);
+	return dataArray;
+}
+
+int MyData::getSize(void){
+	return _ndati;
+}
+
+float* MyData::getData(){
+	return _data;
+}
--- fileToArray.h
+++ fileToArray.h
@@ -1,1 +1,26 @@
+#ifndef FILETOARRAY_H
+#define FILETOARRAY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+class MyData{
+	public:
+	// methods
+	int getSize(void);
+	float* getData(void);
+	// attributes
+	
+	// constructor
+	MyData(const char* filename);
+	private:
+	// methods
+	FILE* myfopen(const char* filename);
+	int countData(const char* filename);
+	float* readData(const char* filename);
+	// attributes
+	int _ndati;
+	float* _data;
+};
+
+#endif
